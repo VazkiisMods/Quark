6aeb232cdd6f044629c2aa9a723c8ae9b0b5f804
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-03-30 18:26:24 -0500

    Maple Chest + Autumnity Homie + Snake Block Frog

--- src/main/java/vazkii/quark/mobs/client/render/FrogRenderer.java
+++ src/main/java/vazkii/quark/mobs/client/render/FrogRenderer.java
@@ -14,6 +14,7 @@ public class FrogRenderer extends MobRenderer<FrogEntity, FrogModel> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation("quark", "textures/model/entity/frog.png");
 	private static final ResourceLocation TEXTURE_SWEATER = new ResourceLocation("quark", "textures/model/entity/events/sweater_frog.png");
 	private static final ResourceLocation TEXTURE_FUNNY = new ResourceLocation("quark", "textures/model/entity/events/funny_rat_frog.png");
+	private static final ResourceLocation TEXTURE_SNAKE = new ResourceLocation("quark", "textures/model/entity/events/snake_block_frog.png");
 
 	public FrogRenderer(EntityRendererManager manager) {
 		super(manager, new FrogModel(), 0.2F);
@@ -21,9 +22,10 @@ public class FrogRenderer extends MobRenderer<FrogEntity, FrogModel> {
 
 	@Override
 	public ResourceLocation getEntityTexture(@Nonnull FrogEntity entity) {
-		if(entity.hasCustomName() && entity.getCustomName().getUnformattedComponentText().trim().equalsIgnoreCase("Alex"))
+		if(entity.hasCustomName() && (entity.getCustomName().getUnformattedComponentText().trim().equalsIgnoreCase("Alex") || entity.getCustomName().getUnformattedComponentText().trim().equalsIgnoreCase("Rat") || entity.getCustomName().getUnformattedComponentText().trim().equalsIgnoreCase("Funny Rat")))
 			return TEXTURE_FUNNY;
-			
+		if(entity.hasCustomName() && (entity.getCustomName().getUnformattedComponentText().trim().equalsIgnoreCase("Snake") || entity.getCustomName().getUnformattedComponentText().trim().equalsIgnoreCase("SnakeBlock") || entity.getCustomName().getUnformattedComponentText().trim().equalsIgnoreCase("Snake Block")))
+			return TEXTURE_SNAKE;
 		return entity.hasSweater() ? TEXTURE_SWEATER : TEXTURE;
 	}
 
